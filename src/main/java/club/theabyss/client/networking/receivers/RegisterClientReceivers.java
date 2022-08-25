91deea20794a2abc670b6dcc4953c167292f59fd
zLofro <lofro2786@gmail.com>
2022-08-25 18:29:12 +0200

    Fixed a runTimeException when registering the FlashBangS2CFlashPacket using a static function instead of the lambda on dedicated servers.

--- src/main/java/club/theabyss/client/networking/receivers/RegisterClientReceivers.java
+++ src/main/java/club/theabyss/client/networking/receivers/RegisterClientReceivers.java
@@ -1,12 +1,27 @@
 package club.theabyss.client.networking.receivers;
 
+import club.theabyss.client.render.flashBang.FlashBangClientManager;
 import club.theabyss.networking.packet.s2c.FlashBangS2CFlashPacket;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gl.SimpleFramebuffer;
 
 public class RegisterClientReceivers {
 
     public static void init() {
-        ClientPlayNetworking.registerGlobalReceiver(FlashBangS2CFlashPacket.ID, FlashBangS2CFlashPacket::registerReceiver);
+        //TODO DO THIS WITH A STATIC FUNCTION FROM THE PACKET INSTEAD OF A LAMBDA.
+        ClientPlayNetworking.registerGlobalReceiver(FlashBangS2CFlashPacket.ID, (client, handler, buf, responseSender) -> {
+            FlashBangClientManager.reset();
+            FlashBangClientManager.setOpacity(buf.readFloat());
+            FlashBangClientManager.setFlashSeconds(buf.readInt());
+            FlashBangClientManager.setOpaqueTicks(buf.readInt());
+            var showStaticImage = buf.readBoolean();
+            MinecraftClient.getInstance().execute(() -> {
+                if (showStaticImage && client.getWindow().getWidth() > 0 && client.getWindow().getHeight() > 0)
+                    FlashBangClientManager.setFramebuffer(FlashBangClientManager.copyColorsFrom(client.getFramebuffer(), new SimpleFramebuffer(client.getWindow().getWidth(), client.getWindow().getHeight(), true, false)));
+                FlashBangClientManager.setShouldTick(true);
+            });
+        });
     }
 
 }
--- src/main/java/club/theabyss/networking/packet/s2c/FlashBangS2CFlashPacket.java
+++ src/main/java/club/theabyss/networking/packet/s2c/FlashBangS2CFlashPacket.java
@@ -1,13 +1,8 @@
 package club.theabyss.networking.packet.s2c;
 
-import club.theabyss.client.render.flashBang.FlashBangClientManager;
 import club.theabyss.global.utils.TheAbyssConstants;
 import lombok.Getter;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
-import net.fabricmc.fabric.api.networking.v1.PacketSender;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gl.SimpleFramebuffer;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.Identifier;
 
@@ -34,19 +29,6 @@ public class FlashBangS2CFlashPacket {
 
     public FlashBangS2CFlashPacket(float opacity, int flashSeconds) {
         this(opacity, flashSeconds, 0, true);
-    }
-
-    public static void registerReceiver(MinecraftClient client, ClientPlayNetworkHandler handler, PacketByteBuf buf, PacketSender responseSender) {
-        FlashBangClientManager.reset();
-        FlashBangClientManager.setOpacity(buf.readFloat());
-        FlashBangClientManager.setFlashSeconds(buf.readInt());
-        FlashBangClientManager.setOpaqueTicks(buf.readInt());
-        var showStaticImage = buf.readBoolean();
-        MinecraftClient.getInstance().execute(() -> {
-            if (showStaticImage && client.getWindow().getWidth() > 0 && client.getWindow().getHeight() > 0)
-                FlashBangClientManager.setFramebuffer(FlashBangClientManager.copyColorsFrom(client.getFramebuffer(), new SimpleFramebuffer(client.getWindow().getWidth(), client.getWindow().getHeight(), true, false)));
-            FlashBangClientManager.setShouldTick(true);
-        });
     }
 
     public PacketByteBuf write() {
